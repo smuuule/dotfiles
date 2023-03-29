209b3bb026f2dfb5defc42e7544050b658a66512
Smule (Samuel Runmark Thunell) <samuel.runmark@gmail.com>
2023-03-29 20:29:58 +0200

    new configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,5 @@ end
 
 set PATH /bin $PATH
 
+#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/smule/.ghcup/bin $PATH # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/smule/.ghcup/bin $PATH # ghcup-env
